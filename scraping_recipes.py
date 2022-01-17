44b96c0c02285497a4d2728515ebb76d0e9aaa57
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2022-01-17 18:36:40 -0500

    test(test_html_file_path): Update file path
    Update the HTML file path for the test.

--- scraping_recipes.py
+++ scraping_recipes.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from recipe_note_scraper import RecipeNoteScraper
 
-test_html_file_path = "/html/test.html"
+test_html_file_path = "html/test.html"
 
 recipe_note_scraper = RecipeNoteScraper(test_html_file_path)
 recipe_note_text = recipe_note_scraper.note_text
