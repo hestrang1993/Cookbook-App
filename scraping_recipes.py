c8b816497652cb21174ca085d29ef337a1e0c421
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2022-01-17 18:26:03 -0500

    test(RecipeNoteScraper)
    Use recipe_note_scraper.RecipeNoteScraper to easily extract
    text from a recipe's note section.

--- scraping_recipes.py
+++ scraping_recipes.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from recipe_note_scraper import RecipeNoteScraper
 
-test_html_file_path = "C:\\Users\\harri\\PycharmProjects\\Cookbook-App\\test.html"
+test_html_file_path = "/html/test.html"
 
 recipe_note_scraper = RecipeNoteScraper(test_html_file_path)
 recipe_note_text = recipe_note_scraper.note_text
 print(recipe_note_text)
+print(type(recipe_note_text))
