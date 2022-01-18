0b2d5becc318bc90257151a7d6575c83338ac9e9
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2022-01-17 19:10:00 -0500

    feat: Add variables
    Add variables for:
    
    1. The tag for the recipe notes.
    2. The class for the recipe notes tag.
    3. The tag used for the recipe title.
    4. The class for the recipe title tag.

--- variables.py
+++ variables.py
@@ -1,2 +1,5 @@
 # -*- coding: utf-8 -*-
-note_span_class = "recipe-notes theme-action-text ng-binding ng-scope"
+note_tag = 'span'
+note_class = "recipe-notes theme-action-text ng-binding ng-scope"
+recipe_title_tag = 'h3'
+recipe_title_class = "recipe-title ng-binding ng-scope"
