b3c29199f9fb25e63cc074ba05f8268dee578cd7
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2022-01-25 17:19:50 -0500

    chore: Remove old/redundant modules
    Remove the first iteration of the HTML scraper.
    Remove the variables module.
    Remove the test module for the first iteration of the HTML scraper.

--- scraping_recipes.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from recipe_note_scraper import RecipeNoteScraper
-
-test_html_file_path = "html/test.html"
-text_root = "C:\\Users\\harri\\PycharmProjects\\Cookbook-App\\txt"
-
-rns = RecipeNoteScraper(test_html_file_path, text_root)
-rns.write_text_file()
--- variables.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-note_tag = 'span'
-note_class = "recipe-notes theme-action-txt ng-binding ng-scope"
-recipe_title_tag = 'h3'
-recipe_title_class = "recipe-title ng-binding ng-scope"
